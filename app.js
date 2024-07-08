8e7b59db19f6a62d790556027b8294a89a180348
Kathiravan B <itsmekathi92@gmail.com>
2024-07-08 22:25:28 +0530

    Test with production connection

--- app.js
+++ app.js
@@ -3,17 +3,12 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 var cors = require('cors')
 const mongoose = require('mongoose');
-
-mongoose.connect(config.get('connectionString'))
-    .then(() => console.log('connected to mongodb'))
-    .catch((err) => console.error('Error connecting to mongodb', err));
-
-const todos = require('./routes/todos');
-const home = require('./routes/home');
-
 const express = require('express')
 const logger = require('./middlewares/logger');
 const auth = require('./middlewares/auth');
+
+const todos = require('./routes/todos');
+const home = require('./routes/home');
 
 const app = express();
 
@@ -37,6 +32,11 @@ console.log(`Application is running in ${app.get('env')} mode`);
 console.log(`Application Name: ${config.get('name')}`);
 console.log(`Mail server: ${config.get('mail.host')}`);
 console.log(`Mail server password: ${config.get('mail.password')}`);
+console.log(`Database connection string: ${config.get('connectionString')}`);
+
+mongoose.connect(config.get('connectionString'))
+    .then(() => console.log('connected to mongodb'))
+    .catch((err) => console.error('Error connecting to mongodb', err));
 
 const port = process.env.port || 3000;
 
